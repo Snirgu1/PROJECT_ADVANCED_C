d9475c8e145a0a6b518ff70bb4e851db65417d16
idanmorem <37801660+idanmorem@users.noreply.github.com>
2020-12-12 18:09:13 +0200

    Update main_commands.h

--- main_commands.h
+++ main_commands.h
@@ -2,12 +2,15 @@
 #define PROJECT_ADVANCED_C_MAIN_COMMANDS_H
 #include "DataBase.h"
 
+void checkCommand(char* line, List* lstByCode, List* lstByPrice);
+
 /* ===== FIND FUNCTIONS =====*/
 void find(List* apartment, char* line);
 
 /* ===== ADD FUNCTIONS =====*/
-apt* CreApt(char* line); /* TEST ONLY !!!! _ NOT EFFICIENT */
-LNode* CreateLnode(apt* apt);
+Apt* CreApt(char* line); /* TEST ONLY !!!! _ NOT EFFICIENT */
+LNode* CreateLnode(Apt* apt);
+void addApt(char* line, List* lstByCode, List* lstByPrice);
 
 /* ===== BUY FUNCTIONS =====*/
 

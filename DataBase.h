4fe908620e71acb5883930c35aa11dd8a9c8730a
idanmorem <37801660+idanmorem@users.noreply.github.com>
2020-12-21 10:12:12 +0200

    Update DataBase.h

--- DataBase.h
+++ DataBase.h
@@ -1,9 +1,15 @@
-#ifndef PROJECT_ADVANCED_C_DATABASE_H
-#define PROJECT_ADVANCED_C_DATABASE_H
+//
+// Created by idanm on 12/19/2020.
+//
+
+#ifndef PROJECT_DATABASE_H
+#define PROJECT_DATABASE_H
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<time.h>
 #include<stdlib.h>
 #include<string.h>
+#include<math.h>
 
 /* Define and MACROS */
 #define Uchar unsigned char
@@ -14,10 +20,26 @@
 #define NUM_OF_DELIM 2
 #define NUM_OF_FIND_COMMANDS 8
 #define INCREASE 1
-#define DECREASE 1
-#define INITIAL (-1)
-
+#define DECREASE (-1)
+#define MAX_INIT 2147483647
+#define INITIAL 0
+#define TRUE 1
+#define MAXROOMS 15
+#define FALSE 0
+#define EXIT_PROG 1
+#define BOOL int
 /* Data structure implementation */
+
+typedef struct FindKeys{
+    short int year,month,day;
+    short int lastXdays_enter;
+    short int minR;
+    short int maxR;
+    short int order;
+    int maxP;
+    int minP;
+}Find_keys;
+
 typedef struct Apartment{        /* apartment details */
     int code ;
     char* address;
@@ -32,43 +54,53 @@ typedef struct LIST{             /* singly linked list struct (using also for so
     struct ListNode* tail;
 }List;
 
-typedef struct ListNode {        /* single linked list node struct */
+typedef struct ListNode {        /* doubly linked list node struct */
     struct ListNode* prev;
     struct ListNode* next;
     Apt* apartment;
-}LNode ;
+}LNode;
 
 typedef struct Commands_List {   /* singly linked list struct (commands) */
-    struct Clnode* head;
-    struct Clnode* tail;
+    struct CListNode* head;
+    struct CListNode* tail;
 }CList;
 
 typedef struct CListNode{      /* commands singly linked list nodes  */
-    struct ListNode* next;
+    struct CListNode* next;
     char* command;
-    int code;
-}Clnode;
+    int commandNum;
+}CLnode;
 
 /* Functions Implementation */
-Apt* AllocateApt(int code, char* address, int price, short int num_of_rooms, short int day, short int month, short int year, time_t Database_entry_date);
+Apt* AllocateApt(char* line);
 LNode *AllocateLNode(Apt *apt);
-List AllocateEmptyList();
 void InitializeList(List* list);
 void AddToEmptyList (List *list, LNode *node);
 void AddToBeginningOfList (List *list, LNode *node);
 void AddToEndOfList (List *list, LNode *node);
 void AddToInnerPlaceInList (LNode *prev, LNode *node);
-LNode* FindPlaceToInsertByX(List *list, Apt* apt);
-void AddToList (List *list, Apt* apt);
+LNode* FindPlaceToInsertByPrice(List *list, Apt* apt);
+void AddToListByPrice (List *list, Apt* apt);
 void DeleteFromInnerPlaceInList(LNode *prev);
+void DeleteFromInnerPlaceInPriceList(LNode *prev);
 void DeallocateListNode(LNode *node);
 void DeleteFromBeginningOfList(List *list);
+void DeleteFromBeginningOfPriceList(List *list);
 void DeleteFromEndOfList(List *list);
-void DeleteFromList (List *list, Apt *apt);
-LNode *FindPlaceToDeleteByX(List *list, Apt* apt);
+void DeleteFromEndOfPriceList(List *list);
+void DeleteFromList (List *list, List *listByPrice, int code);
+void DeleteFromPriceList(List *list, LNode *prev);
+LNode *FindPlaceToDeleteByCode(List *list, int code);
 void PrintList(List *list);
 void PrintData(Apt *apt);
 void ClearList(List *list);
 void* check_malloc (int num_of_bytes);
-
-#endif
+void DeleteFromBeginningOfPList(List *list);
+CLnode* AllocateCLnode(char* command);
+void AddToCList(CList *list, char* command);
+void ClearCList(CList *list);
+void DeleteFromBeginningOfCList(CList *list);
+void DeallocateCListcell(CLnode *cell);
+void PrintCList(CList *list);
+void PrintShortHArray(char **array, CList *list);
+#endif //PROJECT_DATABASE_H

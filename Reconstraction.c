eb1d796c152f5416d5e0f8a6aed587328f6b8d10
Snirgu1 <66010745+Snirgu1@users.noreply.github.com>
2020-12-22 11:05:21 +0200

    Update Reconstraction.c

--- Reconstraction.c
+++ Reconstraction.c
@@ -1,37 +1,58 @@
-//
-// Created by idanm on 12/19/2020.
-//
-
 #include "Reconstraction.h"
 static int commandNum = 1;
 
 void checkReconstraction(char* line, List* lstByCode, List* lstByPrice, char** shortHistory, CList* history)
 {
+    int len ;
+    int num ;
     if(line[0] == '!')
         checkCommand(shortHistory[6], lstByCode, lstByPrice, shortHistory, history);
     else
     {
-            int num = atoi(line);
-            findCommand(line+2, num, lstByCode, lstByPrice, shortHistory, history);
+        num = atoi(line);
+        len = checkNumLength(num);
+        if( *(line + len) == '\0')
+            findCommand(NULL,num, lstByCode, lstByPrice, shortHistory, history);
+        else
+            findCommand(line+len+1, num, lstByCode, lstByPrice, shortHistory, history);
     }
 }
 
-void switchCommand(char* line,char *command, List* lstByCode, List* lstByPrice, char** shortHistory, CList* history)
+int checkNumLength(int num)
 {
-    char *newCommand, *newParameter, *oldParameter, *location;
-    oldParameter = strtok(line, "^");
-    newParameter = strtok(NULL, "^");
-    if(oldParameter == NULL)
+    int  counter = 1 ;
+    while( num > 10 )
+    {
+        counter++;
+        num= num/10;
+    }
+    return counter;
+}
+
+void switchCommand(char *line, char *command, List *lstByCode, List *lstByPrice, char **shortHistory, CList *history)
+{
+    if(line == NULL)
     {
         checkCommand(command, lstByCode, lstByPrice, shortHistory, history);
         return;
     }
-    newCommand = (char*)check_malloc(strlen(command) + strlen(newParameter) - strlen(oldParameter));
+    char *newCommand, *newParameter, *oldParameter, *location, *temp;
+    temp = strdup(line);
+    oldParameter = strtok(temp, "^");
+    newParameter = strtok(NULL, "^");
+    if(oldParameter == NULL)
+    {
+        checkCommand(command, lstByCode, lstByPrice, shortHistory, history);
+        free(temp);
+        return;
+    }
+    newCommand = (char*)check_malloc(strlen(command)+strlen(newParameter)-strlen(oldParameter));
     location = strstr(command, oldParameter);
     strncpy(newCommand, command, strlen(command)-strlen(location));
     strcpy(newCommand+strlen(command)-strlen(location), newParameter);
     strcat(newCommand, location+strlen(oldParameter));
     checkCommand(newCommand, lstByCode, lstByPrice, shortHistory, history);
+    free(temp);
 }
 
 void findCommand(char *line, int num, List* lstByCode, List* lstByPrice, char** shortHistory, CList* history)
@@ -39,11 +60,12 @@ void findCommand(char *line, int num, List* lstByCode, List* lstByPrice, char** 
     CLnode *curr = history->head;
     while(curr != NULL)
     {
-            if(curr->commandNum == num) {
-                switchCommand(line, curr->command, lstByCode, lstByPrice, shortHistory, history);
-                break;
-            }
-            curr = curr->next;
+        if(curr->commandNum == num)
+        {
+            switchCommand(line, curr->command, lstByCode, lstByPrice, shortHistory, history);
+            break;
+        }
+        curr = curr->next;
     }
     if(curr == NULL)
         switchCommand(line, shortHistory[N-(commandNum-num)], lstByCode, lstByPrice, shortHistory, history);

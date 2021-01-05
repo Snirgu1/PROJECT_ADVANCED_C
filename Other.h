21dff100bd9c52fe6504b2460c53653ea37cda70
idanmorem <37801660+idanmorem@users.noreply.github.com>
2021-01-05 09:43:57 +0200

    Update Other.h

--- Other.h
+++ Other.h
@@ -1,28 +1,13 @@
-#ifndef PROJECT_ADVANCED_C_OTHER_H
-#define PROJECT_ADVANCED_C_OTHER_H
-#include "DataBase.h"
+//
+// Created by idanm on 12/19/2020.
+//
 
-/* get line function */
-char* get_line()
-{
-    char ch = ' ' ;
-    int l_size = 0, p_size = 2;
-    char* line = (char*)malloc(sizeof(char)*p_size);
-    while( ch != '\n')
-    {
-        scanf("%c",&ch);
-        line[l_size] = ch;
-        l_size++;
-        if(l_size == p_size)
-        {
-            p_size *= 2;
-            line = (char*)realloc(line,p_size);
-        }
-    }
-    line = (char*)realloc(line,l_size+1);
-    line[l_size] = '\0';
-    return line;
-}
-void AllocError();
+#ifndef PROJECT_OTHER_H
+#define PROJECT_OTHER_H
 
-#endif
+#include <stdio.h>
+
+void* check_malloc (int num_of_bytes);
+void check_file (FILE *file);
+char *inputString(FILE* fp, size_t size);
+#endif //PROJECT_OTHER_H

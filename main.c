d21782cbb382421bd9c7a42334b4b385c3d38247
idanmorem <37801660+idanmorem@users.noreply.github.com>
2021-01-05 09:44:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,20 @@
 #include "main_commands.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main( )
 {
-    printf("Please enter ....\n");
-    List lst; lst.head = lst.tail = NULL ;
-    LNode*n1,*n2,*n3,*n4,*curr;
-    char* temp;
+    List lst, lstPrice;
+    CList history;
+    char *short_term_history[N];
+    InitializeDataBase(&lst,&lstPrice,short_term_history,&history);          /* initialize required Lists and array */
+//    ReadBinFile("DB_Binfile.bin",&lst,&lstPrice);                      /* reading Bin file and set info to DB  */
+    TestProject(&lst,&lstPrice,short_term_history,&history);                  /* Project tests */
+//    getHistoryFromFile(short_term_history,&history);
+//    TestProjectAfterFiles(&lst,&lstPrice,short_term_history,&history);       /* Project tests part two - after we got files filled  */
+    WriteHistoryToFile(&history,short_term_history);                         /* writing history to txt file */
+    WriteDatabaseToBinFile(&lst);/* writing apartments DB to Bin file */
+    ClearDB(&history, short_term_history, &lst, &lstPrice);
 
-    temp = "add-apt \"Hasavyonim 23 Ramat Ono\" 2000000 4 01 01 21";
-    apt* apt1 = CreApt(temp);
-    apt1->code = 1;
-    n1 = CreateLnode(apt1);
-    lst.head = n1;
-
-    temp = "add-apt \"Rothschild 67 Rishon Le Zion\" 1700000 3 03 10 20";
-    apt* apt2 = CreApt(temp);
-    apt2->code = 2;
-    n2 = CreateLnode(apt2);
-    n1->next = n2 ;
-
-
-    n2->prev = n1 ;
-
-    temp = "add-apt \"Ben Gurion 25 Herzliya\" 2200000 5 01 08 20";
-    apt* apt3 = CreApt(temp);
-    apt3->code = 3;
-    n3 = CreateLnode(apt3);
-    n2->next = n3 ;
-    n3->prev = n2 ;
-
-    temp = "add-apt \"Gordon 85 Holon\" 1500000 3 15 06 21";
-    apt* apt4 = CreApt(temp);
-    apt4->code = 4;
-    n4 = CreateLnode(apt4);
-    n3->next = n4 ;
-    n4->prev = n3 ;
-
-    lst.tail = n4;
-//    for(curr = lst.head ; curr != NULL ; curr=  curr->next )
-//    {
-//        printf("%d\n",curr->apartment->price);
-//    }
-    temp ="find-apt -MinNumRooms 3 -MaxNumRooms 5 -MaxPrice 1750000 -s";
-    find(&lst,temp);
     return 0;
 }
-
-

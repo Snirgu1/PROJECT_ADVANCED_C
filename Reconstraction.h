05b6d343b91901adbdf4115712f9c6757b33c2a3
idanmorem <37801660+idanmorem@users.noreply.github.com>
2021-01-05 09:44:36 +0200

    Update Reconstraction.h

--- Reconstraction.h
+++ Reconstraction.h
@@ -1,5 +1,13 @@
-#ifndef PROJECT_ADVANCED_C_RECONSTRACTION_H
-#define PROJECT_ADVANCED_C_RECONSTRACTION_H
-#include "DataBase.h"
 
+#ifndef PROJECT_RECONSTRACTION_H
+#define PROJECT_RECONSTRACTION_H
+#include "main_commands.h"
+#include "DataBase.h"
+void InitializeDataBase(List *lst,List* lstPrice,char** short_term_history,CList* history);
+void checkReconstraction(char* line, List* lstByCode, List* lstByPrice, char** shortHistory, CList* history);
+void InitializeSHistory(char** shortHistory);
+void recordHistory(char* line, char** shortHistory, CList* history);
+void findCommand(char *line, int num, List* lstByCode, List* lstByPrice, char** shortHistory, CList* history);
+void switchCommand(char* line,char *command, List* lstByCode, List* lstByPrice, char** shortHistory, CList* history);
+int checkNumLength(int num);
 #endif
